385936df1369f3e6b08276b767c6efb63bc10cae
iamDayoDev <iamdayoaderinto@gmail.com>
2026-05-11 09:57:28 +0100

    fix: modify terraform script to use tfvars

---
+++ terraform/environment/dev.tfvars
@@ -0,0 +1,17 @@
+aws_region  = "us-east-1"
+cluster_name = "petclinic-eks"
+environment  = "dev"
+app_namespace = "petclinic"
+node_instance_type = "t3.small"
+node_desired_size = 2
+node_min_size = 1
+node_max_size = 3
+
+domain = "etaoko.com"
+eso_role_name  = "petclinic-eso-role"
+alb_role_name  = "petclinic-alb-role"
+alb_policy_name = "AWSLoadBalancerControllerIAMPolicy"
+
+cluster_admin_principal_arns = [
+  "arn:aws:iam::979779072306:user/Aderinto",
+]
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,17 @@
-variable "aws_region" { default = "us-east-1" }
-variable "cluster_name" { default = "petclinic-eks" }
-variable "environment" { default = "production" }
+variable "aws_region" {
+  description = "AWS region where the cluster and Secrets Manager live"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+
+}
+variable "environment" { 
+  description = "Deployment environment (e.g. dev, staging, prod)"
+  type        = string
+ }
 variable "app_namespace" {
   description = "Namespace used by the Petclinic application"
   type        = string
@@ -8,29 +19,36 @@ variable "app_namespace" {
 variable "node_instance_type" {
   description = "EC2 instance type for the EKS managed node group"
   type        = string
-  default     = "t3.small"
 }
 variable "node_desired_size" {
   description = "Desired number of nodes in the EKS managed node group"
   type        = number
-  default     = 2
 }
 variable "node_min_size" {
   description = "Minimum number of nodes in the EKS managed node group"
   type        = number
-  default     = 1
 }
 variable "node_max_size" {
   description = "Maximum number of nodes in the EKS managed node group"
   type        = number
-  default     = 3
 }
-variable "domain" { default = "eta-oko.com" }
-variable "eso_role_name" { default = "petclinic-eso-role" }
-variable "alb_role_name" { default = "petclinic-alb-role" }
-variable "alb_policy_name" { default = "AWSLoadBalancerControllerIAMPolicy" }
+variable "domain" {
+  description = "Domain name for the application"
+  type        = string
+}
+variable "eso_role_name" {
+  description = "Name of the role for External Secrets Operator"
+  type        = string
+}
+variable "alb_role_name" {
+  description = "Name of the role for AWS Load Balancer Controller"
+  type        = string
+}
+variable "alb_policy_name" {
+  description = "Name of the policy for AWS Load Balancer Controller"
+  type        = string
+}
 variable "cluster_admin_principal_arns" {
   description = "IAM principal ARNs to grant cluster-admin access to EKS via access entries"
   type        = set(string)
-  default     = ["arn:aws:iam::979779072306:user/Aderinto"]
 }
